c8e4dd354358717756a39922d82c51d58900c7b8
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-04-05 23:40:39 -0500

    app.js and login form refactors

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,37 +25,37 @@ export const ADD_USER = gql`
 `;
 
 export const ADD_BOOK = gql`
-    mutation Mutation($userId: ID!, $book: String!) {
-        addBook(userId: $userId, book: $book) {
-            _id
-            savedBooks {
-                _id
-                bookId
-                title
-                description
-                authors
-                image
-                link
-            }
-            bookCount
-        }
+  mutation addBook($userId: ID!, $book: String!) {
+    addBook(userId: $userId, book: $book) {
+      _id
+      savedBooks {
+        _id
+        bookId
+        title
+        description
+        authors
+        image
+        link
+      }
+      bookCount
     }
+  }
 `
 
 export const REMOVE_BOOK = gql`
-    mutation RemoveBook($userId: ID!, $bookId: ID!) {
-        removeBook(userId: $userId, bookId: $bookId) {
-            _id
-            savedBooks {
-                _id
-                authors
-                bookId
-                description
-                image
-                link
-                title
-            }
-            bookCount
-        }
+  mutation removeBook($userId: ID!, $bookId: ID!) {
+    removeBook(userId: $userId, bookId: $bookId) {
+      _id
+      savedBooks {
+        _id
+        authors
+        bookId
+        description
+        image
+        link
+        title
+      }
+      bookCount
     }
+  }
 `
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,11 +1,11 @@
 import { gql } from '@apollo/client';
 
 export const QUERY_USER = gql`
-    query User($id: ID!) {
-        user(_id: $id) {
-            username
-            email
-            password
-        }
+  query user($id: ID!) {
+    user(_id: $id) {
+      username
+      email
+      password
     }
+  }
 `;
